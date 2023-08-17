1a79eb288511a50284fb4e6da372695d144ae121
Howard van Rooijen <howard.vanrooijen@endjin.com>
2023-08-17 11:32:23 +0100

    Add the ability to read the file types. (#4)

--- Solutions/TeamsTranscript.Abstractions/TeamsTranscript/Abstractions/Persistence/TranscriptJsonPersistence.cs
+++ Solutions/TeamsTranscript.Abstractions/TeamsTranscript/Abstractions/Persistence/TranscriptJsonPersistence.cs
@@ -13,4 +13,19 @@ public class TranscriptJsonPersistence : ITranscriptPersistence
 
         await File.WriteAllTextAsync(Path.ChangeExtension(outputFilePath!.FullName, ".json"), JsonSerializer.Serialize(transcripts, new JsonSerializerOptions { WriteIndented = true })).ConfigureAwait(false);
     }
+
+    public async Task<IEnumerable<Transcription>> RetrieveAsync(FileInfo inputFilePath)
+    {
+        if (inputFilePath is null)
+        {
+            throw new ArgumentNullException(nameof(inputFilePath));
+        }
+
+        if (!inputFilePath.Exists)
+        {
+            throw new FileNotFoundException($"The file {inputFilePath.FullName} does not exist.", inputFilePath.FullName);
+        }
+
+        return JsonSerializer.Deserialize<IEnumerable<Transcription>>(await File.ReadAllTextAsync(inputFilePath.FullName).ConfigureAwait(false))!;
+    }
 }
